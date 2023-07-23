e2c65ae97015544bfbc1ff33f2294450ae27decd
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-23 20:58:45 +0200

    Create DTO for Student Register End Point

---
+++ TPH/DTOs/StudentRegisterDto.cs
@@ -0,0 +1,16 @@
+﻿namespace TPH.DTOs
+{
+    public class StudentRegisterDto
+    {
+        public string UserName { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+    }
+}
