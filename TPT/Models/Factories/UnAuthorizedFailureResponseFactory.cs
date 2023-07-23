ccf5d4bbe2a8337872c1099074f14d6704138c0b
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-23 23:25:56 +0200

    Add Factories and their Responses for Factory Design Pattern

---
+++ TPT/Models/Factories/UnAuthorizedFailureResponseFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPT.Interfaces;
+using TPT.Models.Responses;
+
+namespace TPT.Models.Factories
+{
+    public class UnAuthorizedFailureResponseFactory : IResponseFactory
+    {
+        private readonly UnAuthorizedFailureResponse _response;
+
+        public UnAuthorizedFailureResponseFactory()
+        {
+            _response = new UnAuthorizedFailureResponse();
+        }
+        public IResponse CreateResponse()
+        {
+            return _response;
+        }
+    }
+}
---
+++ TPT/Models/Responses/FailureResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPT.Interfaces;
+
+namespace TPT.Models.Responses
+{
+    public class FailureResponse : IResponse
+    {
+        public bool Status { get; private set; }
+
+        public int StatusCode { get; private set; }
+
+        public string? Message { get; set; }
+
+        public FailureResponse(int statusCode, string? message = null)
+        {
+            Status = false;
+            StatusCode = statusCode;
+            Message = message;
+        }
+    }
+}
