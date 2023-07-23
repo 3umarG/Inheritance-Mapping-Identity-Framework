767c92a98195c8d34dd4a29295da67c0a9dffe77
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-23 22:41:56 +0200

    Create \`TeacherRegisterDto\`

---
+++ TPH/DTOs/TeacherRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TPH.DTOs
+{
+	public class TeacherRegisterDto : LoginDto
+	{
+		public int Age { get; set; }
+
+		public string Degree { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
