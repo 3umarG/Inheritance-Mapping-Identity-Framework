d222efbe1a4d16c23a67389b74b2047c36f8487f
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-23 21:00:05 +0200

    Implement MapperProfiler and AuthService

---
+++ TPH/Services/AuthService.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TPH.Data;
+using TPH.DTOs;
+using TPH.Interfaces;
+using TPH.Models;
+using TPH.Models.Auth;
+
+namespace TPH.Services
+{
+	public class AuthService : IAuthService
+	{
+		private readonly UserManager<User> _userManager;
+		private readonly IMapper _mapper;
+		private readonly JWT _jwt;
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public AuthService(
+			UserManager<User> userManager,
+			IMapper mapper,
+			IOptions<JWT> jwt,
+			RoleManager<IdentityRole> roleManager)
+		{
+			_userManager = userManager;
+			_mapper = mapper;
+			_jwt = jwt.Value;
+			_roleManager = roleManager;
+		}
+		public async Task<AuthModel> RegisterStudent(StudentRegisterDto dto)
+		{
+			var auth = new AuthModel();
+
+			var existedUser = await _userManager.FindByNameAsync(dto.UserName);
+			if(existedUser is not null)
+			{
+				auth.Message = "There is already User with the same user name";
+				return auth;
+			}
+
+			var user = _mapper.Map<Student>(dto);
+			user.Email = dto.UserName;
+			var result = await _userManager.CreateAsync(user ,dto.Password);
+
+			if (!result.Succeeded)
+			{
+				auth.Message = "Error occured while register !!";
+				return auth;
+			}
+
+			auth.IsAuthed = true;
+			auth.Email = user.Email;
+			auth.UserName = user.UserName;
+
+			return auth;
+		}
+	}
+}
---
+++ TPH/Services/MappingProfiler.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TPH.DTOs;
+using TPH.Models;
+
+namespace TPH.Services
+{
+	public class MappingProfiler : Profile
+	{
+        public MappingProfiler()
+        {
+            CreateMap<Student, StudentRegisterDto>()
+                .ReverseMap();
+        }
+    }
+}
